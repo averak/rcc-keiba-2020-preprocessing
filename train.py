9f5d57f324808c0f79ff6e60acd78e4978317557
averak <abe12@mccc.jp>
2020-10-11 01:43:55 +0900

    [update] 前処理をpreprocessing.pyに委譲

--- train.py
+++ train.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
 import nnet
-import csv
+import preprocessing
 
-x = []
-y = []
-
-with open('weather.csv', 'r') as f:
-    reader = csv.reader(f)
-
-    for row in list(reader)[1:]:
-        label = int(row[9] == '有')
-
-        row.pop(0)
-        row.pop(-1)
-        row.pop(-1)
-
-        x.append(row)
-        y.append(label)
-
-x = np.array(x, dtype=np.float32)
-y = np.array(y)
+x = preprocessing.FEATURE[:, :8]
+y = preprocessing.FEATURE[:, 8]
 
 model = nnet.nnet(x.shape[1:])
 model.compile(
@@ -33,6 +17,6 @@ model.fit(
     x,
     y,
     batch_size=32,
-    epochs=5,
+    epochs=10,
     validation_split=0.1,
 )
