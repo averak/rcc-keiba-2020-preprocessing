40b8a47052463f6a2510768b82a8bcb7dde99ac1
Fukada Hiroki <is0521ii@ed.ritsumei.ac.jp>
2020-10-25 12:51:19 +0900

    [update] PCAを実装

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-
 def is_rain(x):
     return int(x == '有')
-
 
 def wind_direction(x):
     azimuth = {
@@ -15,7 +13,6 @@ def wind_direction(x):
         '北': 360,
     }
     return sum([azimuth[c] for c in x]) / len(x)
-
 
 # csv読み込み
 df = pd.read_csv('weather.csv')
@@ -31,5 +28,19 @@ scaler.fit(df[df.columns[:8]])
 df[df.columns[:8]] = pd.DataFrame(scaler.transform(
     df[df.columns[:8]]), columns=df.columns[:8])
 
-# 消さないで！
-FEATURE = np.array(df)
+# ==========================================
+# 今回のコード（PCA）
+from sklearn.decomposition import PCA
+
+# データセットの準備
+feature = np.array(df)
+X = feature[:, :8]
+Y = feature[:, 8]
+
+# 主成分分析の実行
+pca = PCA(n_components=4)
+pca.fit(X)
+
+# 次元削減
+X = pca.transform(X)
+# ==========================================
